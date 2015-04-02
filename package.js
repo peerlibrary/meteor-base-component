62ecc8375a1c9380a982e5784616b443a101e1a1
Mitar <mitar.git@tnode.com>
2015-04-01 22:07:40 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:base-component',
   summary: "Base component for reusable Meteor components",
-  version: '0.1.0',
+  version: '0.2.0',
   git: 'https://github.com/peerlibrary/meteor-base-component.git',
   documentation: null
 });
